92f034a627ea7c403337578de671e88692bbaa28
Alex Bota <botavalex@gmail.com>
2021-05-02 15:09:32 +0200

    fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,4 @@
 import * as React from 'react'
-// import layouts
-import DefaultLayout from '../components/layouts'
 // import components
 import { Link } from 'gatsby'
 import { Button } from 'primereact/button'
@@ -8,17 +6,15 @@ import { withUnpublishedPreview } from 'gatsby-source-prismic'
 import Blog from '../templates/Blog'
 
 const Page404 = () => (
-  <DefaultLayout>
-    <section className="section-error p-d-flex p-flex-column p-jc-center p-ai-center">
-      <h1 style={{ fontSize: '2rem' }}>Oops! It's a dead end</h1>
-      <Link className="go-back-btn" to="/">
-        <Button
-          label="Back Home"
-          className="p-button-rounded p-button-warning p-button-lg"
-        />
-      </Link>
-    </section>
-  </DefaultLayout>
+  <section className="section-error p-d-flex p-flex-column p-jc-center p-ai-center">
+    <h1 style={{ fontSize: '2rem' }}>Oops! It's a dead end</h1>
+    <Link className="go-back-btn" to="/">
+      <Button
+        label="Back Home"
+        className="p-button-rounded p-button-warning p-button-lg"
+      />
+    </Link>
+  </section>
 )
 
 export default withUnpublishedPreview(Page404, {
